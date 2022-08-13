68e9919c164a685f56d9746543228b56f4c015b1
wordandahalf <10679776+wordandahalf@users.noreply.github.com>
2022-08-13 09:44:10 -0500

    Fix build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.4.30"
     id("org.openjfx.javafxplugin") version "0.0.10"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
 group = "org.wordandahalf"
@@ -11,35 +12,25 @@ repositories {
 }
 
 javafx {
-    version = "11.0.2"
+    version = "16"
     modules("javafx.controls", "javafx.fxml")
 }
 
 dependencies {
     implementation("no.tornado:tornadofx:1.7.20")
-    implementation("br.com.gamemods:nbt-manipulator:2.0.0")
-    implementation("com.github.luben:zstd-jni:1.5.0-2")
+    implementation("br.com.gamemods:nbt-manipulator:3.1.0")
+    implementation("com.github.luben:zstd-jni:1.5.2-2")
 }
 
 tasks {
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions { jvmTarget = "11" } }
-}
-
-tasks.register<Jar>("uberJar") {
-    archiveClassifier.set("uber")
-
-    manifest {
-        attributes(
-            "Main-Class" to "org.wordandahalf.slimedit.SlimeditMainKt",
-            "Implementation-Title" to "Gradle",
-            "Implementation-Version" to archiveVersion
-        )
+    jar {
+        manifest {
+            attributes(
+                "Main-Class" to "org.wordandahalf.slimedit.SlimeditMainKt",
+                "Implementation-Title" to "Gradle",
+                "Implementation-Version" to archiveVersion
+            )
+        }
     }
-
-    from(sourceSets.main.get().output)
-
-    dependsOn(configurations.runtimeClasspath)
-    from({
-        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-    })
 }
